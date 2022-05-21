3aad78aadf5371edfb3cf9ec543299de7f6cc2e6
Adriano <adriano_alves_2008@hotmail.com>
2022-05-21 14:10:39 -0300

    criando itens

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -4,6 +4,7 @@ import Buscador from './buscador'
 import { useState } from 'react'
 import Filtros from './filtros'
 import Ordenador from './ordenador'
+import Itens from './itens'
 
 export default function Cardapio (){
   const [busca, setBusca] = useState("")
@@ -28,6 +29,7 @@ export default function Cardapio (){
           <Filtros filtro={filtro} setFiltro={setFiltro}/>
           <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
         </div>
+        <Itens />
       </section>
     </main>
   )
---
+++ src/pages/cardapio/itens/index.tsx
@@ -0,0 +1,15 @@
+import cardapio from './itens.json'
+import Item from './item'
+import styles from './Itens.module.scss'
+
+export default function Itens (){
+  return (
+    <div className={styles.itens}>
+      {cardapio.map(item => (
+        <div>
+          <Item key={item.id}/>
+        </div>
+      ))}
+    </div>
+  )
+}
