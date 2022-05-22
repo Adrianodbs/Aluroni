8a9d5786085d38c68924230d74133be934cfe34f
Adriano <adriano_alves_2008@hotmail.com>
2022-05-22 14:25:42 -0300

    eslint

---
+++ src/pages/inicio/index.tsx
@@ -0,0 +1,12 @@
+import cardapio from "pages/cardapio/itens/itens.json";
+
+export default function Inicio (){
+  return (
+    <section>
+      <h3>
+        Recomendações da cozinha
+      </h3>
+      <div></div>
+    </section>
+  );
+}
