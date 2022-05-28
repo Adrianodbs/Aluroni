30ecb23880e08d0d9f9abc1a39830997a1122764
Adriano <adriano_alves_2008@hotmail.com>
2022-05-28 20:18:49 -0300

    pagina padrão

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,18 @@ import Inicio from 'pages/inicio'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import Menu from 'components/menu'
-import styles from './Routes.module.scss'
+import PaginaPadrao from 'components/pagina-padrao'
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <Menu />
-        <header className={styles.header}>
-          <div className={styles.header__text}>A casa do código e da massa</div>
-        </header>
         <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/cardapio" element={<Cardapio />} />
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+          </Route>
         </Routes>
       </Router>
     </main>
