36257a6bce7a18cc2e211564dc69729ee8e50d4f
Adriano <adriano_alves_2008@hotmail.com>
2022-05-29 17:36:43 -0300

    criando o Not Found

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import React from 'react'
 import Menu from 'components/menu'
 import PaginaPadrao from 'components/pagina-padrao'
 import Sobre from 'pages/sobre'
+import Footer from 'components/footer'
+import NotFound from 'pages/NotFound'
 
 export default function AppRouter() {
   return (
@@ -17,7 +19,9 @@ export default function AppRouter() {
             <Route path="cardapio" element={<Cardapio />} />
             <Route path="sobre" element={<Sobre />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
   )
