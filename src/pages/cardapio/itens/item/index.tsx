6021f5cca0bf84f04f92d6ec1f38d5b8baf1b87f
Adriano <adriano_alves_2008@hotmail.com>
2022-06-02 16:33:03 -0300

    notfound na tela de detalhes

--- src/pages/cardapio/itens/item/index.tsx
+++ src/pages/cardapio/itens/item/index.tsx
@@ -2,12 +2,14 @@ import styles from './Item.module.scss';
 import React from 'react';
 import { Prato } from 'types/prato';
 import TagsPrato from 'components/tagsPrato';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Item (props:Prato){
-  const {title, description, photo } = props;
+  const {id, title, description, photo } = props;
+  const navigate = useNavigate();
   return(
-    <div className={styles.item}>
+    <div className={styles.item} onClick={()=> navigate(`/prato/${id}`)}>
       <div className={styles.item__imagem}>
         <img src={photo} alt={title} />
       </div>
